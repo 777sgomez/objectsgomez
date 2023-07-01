26bd913ae20b92b67ce84e4b8930de9fed84c036
Susan Gomez <susangomez@Susans-MBP.attlocal.net>
2023-07-01 11:54:11 -0400

    Added a const movies and added a name key, year key, director key, and picture key all containing info about Twillight to objects.js

--- objects.js
+++ objects.js
@@ -1,1 +1,9 @@
 const ol = document.querySelector("ol");
+const movies = [
+    {
+        name: "Twilight",
+        year: 2008,
+        director: "Chris Weitz",
+        picture: "https://m.media-amazon.com/images/M/MV5BMTQ2NzUxMTAxN15BMl5BanBnXkFtZTcwMzEyMTIwMg@@._V1_.jpg",
+    },
+]
